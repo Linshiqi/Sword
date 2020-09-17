a2a8f8e58d1d2f3238c185f72f8aab4f154f128a
linshiqi <lin047@foxmail.com>
2020-09-17 17:08:53 +0800

    模板类的定义需要在同一个文件

--- src/p6/PrintLinkReverse.h
+++ src/p6/PrintLinkReverse.h
@@ -45,4 +45,16 @@ namespace code047 {
 		template<typename T>
 		void printReverse(std::shared_ptr<ListNode<T>> head_ptr);
 	};
+	template<typename T>
+	void PrintLinkReverse::printReverse(std::shared_ptr<ListNode<T>> head_ptr) {
+		if (head_ptr == nullptr) {
+			return;
+		}
+		if (head_ptr->next != nullptr) {
+			this->printReverse(head_ptr->next);
+		}
+		std::cout << head_ptr->value << " ";
+	}
 }
+
+
